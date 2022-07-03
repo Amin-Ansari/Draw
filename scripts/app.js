d2a321828fe9ca0f19ea185206add8e6930ef1bb
Amin Ansari <devansari@yahoo.com>
2022-07-04 02:59:33 +0430

    We can choose more than one winner, now

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,9 @@
 let randomButton = document.querySelector("button");
 let lists = document.querySelectorAll("ul");
+let theNumberInput = document.querySelector(".winner-number");
+let winners = {
+  winnerId: [],
+};
 
 window.addEventListener("load", showAllTheUsers);
 randomButton.addEventListener("click", showTheWiner);
@@ -25,29 +29,30 @@ function createLi() {
   return document.createElement("li");
 }
 function showTheWiner() {
-  let randomId = Math.floor(Math.random() * 101);
-  if (randomId == 0) {
-    randomId++;
-  }
-  let xhr = new XMLHttpRequest();
-  xhr.open("GET", `https://jsonplaceholder.typicode.com/posts`);
-  xhr.onload = function () {
-    if (xhr.status == 200) {
-      let allTheUsers = this.responseText;
-      allTheUsers = JSON.parse(allTheUsers);
-      for (let element of allTheUsers) {
-        if (element.id === randomId) {
-          lists[0].innerHTML = "";
-          let li = createLi();
-          li.innerHTML += `The user's id: ${element.id} <br />`;
-          li.innerHTML += `The user's title: ${element.title} <br />`;
-          lists[0].appendChild(li);
-          if (!lists[0].classList.contains("show")) {
-            lists[0].classList.add("show");
+  for (let i = 1; i <= Number(theNumberInput.value); i++) {
+    let randomId = Math.floor(Math.random() * 101);
+    if (randomId == 0) {
+      randomId++;
+    }
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://jsonplaceholder.typicode.com/posts`);
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        let allTheUsers = this.responseText;
+        allTheUsers = JSON.parse(allTheUsers);
+        for (let element of allTheUsers) {
+          if (element.id === randomId) {
+            let li = createLi();
+            li.innerHTML += `The user's id: ${element.id} <br />`;
+            li.innerHTML += `The user's title: ${element.title} <br />`;
+            lists[0].appendChild(li);
+            if (!lists[0].classList.contains("show")) {
+              lists[0].classList.add("show");
+            }
           }
         }
       }
-    }
-  };
-  xhr.send();
+    };
+    xhr.send();
+  }
 }
